7f503c63cc677b1f27ec71ef080c4545b38aba38
ryuskd <ryuskd@live.jp>
2023-07-15 13:27:42 +0900

    とりあえずコミット

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+// debug
+ini_set('display_errors', 'On');
+
+// include db_connect.php
+require 'db_connect.php';
+
+// include function.php
+require 'function.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav id="verticalnav">
+        <ul>
+            <li></li>
+        </ul>
+    </nav>
+    <nav id="horizontalnav"></nav>
+    <div id="main">
+        <div class="topic"></div>
+    </div>
+</body>
+
+</html>
